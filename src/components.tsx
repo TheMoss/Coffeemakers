6547edbe69d2a08cf1757d96ae229122a7d7e56e
TheMoss <milena.k98@gmail.com>
2023-05-19 13:48:47 +0200

    Add coffee options, image container components. Give each component a class name to apply styling easier.

--- src/components.tsx
+++ src/components.tsx
@@ -1,31 +1,66 @@
-import * as React from 'react';
-
+import React from 'react';
+import backgroundImage from './background.jpeg';
 
 function Banner() {
     return (
-        <div>Do you want coffee?</div>
-    )
-}
-function DescriptionSection({ special, preparation }: {special:string, preparation:string}) {
-    return (
-        <>
-            <TextFrame text={special} />
-            <TextFrame text={preparation} />
-        </>
+        <div className="banner">Do you want coffee?</div>
     )
 }
 
-function TextFrame({ text }: {text: string }) {    
+function TextFrame({ text }: { text: string }) {
     return (
-        <div>{text}</div>
+        <div className="text-frame">{text}</div>
     )
 }
+function DescriptionSection({ special, preparation }: { special: string, preparation: string }) {
+    return (
+        <div className="choice-section" style={{
+            gridColumn: 4, gridRow: 2
+        }}>
+            <TextFrame text={special} />
+            <TextFrame text={preparation} />
+        </div>)
+}
+
+function CoffeeOptions() {
+    return (
+        <div className="choice-section" style={{ gridColumn: 2, gridRow: 2 }}>
+            <div className='coffee-types'>Type of coffee
+                <div>Strength</div>
+
+                <div>Milk</div>
+
+                <div>Foam</div>
+
+                <div>Sweetness</div>
+
+            </div>
+        </div>
+    )
+}
+
+function ImageContainer() {
+    return (
+        <div style={{gridColumn:3, gridRow:2} }>
+            <div className="circle">
+            </div>
+        </div>)
+}
+
+
 
 
 export function Coffeemakers({ special, preparation }: { special: string, preparation: string }) {
     return (
-        <div>
-            <Banner />
-            <DescriptionSection special={special} preparation={preparation }  />
-        </div>)
+        <html style={{ backgroundImage: `url(${backgroundImage})` }}>
+            <body>
+                <Banner />
+                <div className="App">
+                    <CoffeeOptions />
+                    <ImageContainer/>
+                    <DescriptionSection special={special} preparation={preparation} />
+                </div>
+            </body>
+        </html>
+    )
 }
